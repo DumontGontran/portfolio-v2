c54fea27560e18a3dccd684bd5f9549dad517ffe
DumontGontran <dumont.gontran@orange.fr>
2023-09-03 22:37:02 +0200

    only one webpage that regroups all portfolio informations

--- src/pages/Presentation.tsx
+++ src/pages/Presentation.tsx
@@ -1,76 +1,64 @@
+import { FaHtml5, FaCss3, FaSass } from 'react-icons/fa'
+import { SiJavascript, SiTypescript } from 'react-icons/si'
+import Cards from '../components/Card/Cards'
 import Footer from '../components/Footer/Footer'
-import Header from '../components/Header/Header'
 import './index.css'
 import photo from '../images/dumont-gontran.jpg'
 
 const Presentation = () => {
     return (
         <>
-            <Header />
             <main>
                 <section>
-                    <h2>DUMONT GONTRAN</h2>
+                    <h1>Développeur Front-end</h1>
                     <div className='presentation'>
-                        <div className='description'>
-                            <p className='description_element'>
-                                Je suis un Développeur Web agé de 30 ans.
-                                Je suis motivé et je possède une bonne maîtrise des outils et technologies les plus récents.
-                            </p>
-                            <p className='description_element'>
-                                Mon expérience et mes compétences me permettent de fournir des solutions web modernes,
-                                efficaces et à la pointe de la technologie.
-                                Fort de mon savoir-faire et de mon professionnalisme,
-                                je suis prêt à relever de nouveaux défis et à travailler avec des équipes dévouées.
-                            </p>
-                        </div>
-                        <img src={photo} alt='dumont-gontran' className='photo' />
                         <ul className='description'>
-                            <p className='description_element'>
-                                Mes compétences personnelles comprennent la curiosité, la rigueur, la persévérance, l’esprit d’équipe,
-                                l’intérêt pour le métier, l’adaptabilité et l’amour de l’apprentissage.
-                            </p>
-                            <p className='description_element'>
-                                J'ai travaillé sur de nombreux projets variés, allant d'un site spécialisé dans le partage de cocktails utilisant React JS 18,
-                                à un site e-commerce de restauration avec commande en ligne utilisant SASS,
-                                jusqu'à optimiser le SEO avec un site d'agence en ligne offrant des services de SEO.
-                            </p>
+                            <li className='description_element'>
+                                Je m'appelle <strong>Gontran DUMONT</strong>.
+                                Je suis motivé et je possède <strong>une bonne maîtrise des outils et technologies</strong> les plus récents.
+                            </li>
+                            <li className='description_element'>
+                                Mon expérience et mes compétences me permettent de <strong>fournir des solutions web modernes,
+                                    efficaces et à la pointe de la technologie</strong>.
+                                Fort de mon <strong>savoir-faire</strong> et de mon <strong>professionnalisme</strong>,
+                                je suis prêt à relever de nouveaux défis et à travailler avec des équipes dévouées.
+                            </li>
+                        </ul>
+                        <img src={photo} alt='dumont-gontran' className='photo description_element' />
+                        <ul className='description'>
+                            <li className='description_element'>
+                                Mes compétences personnelles comprennent <strong>la curiosité, la rigueur, la persévérance, l’esprit d’équipe,
+                                    l’intérêt pour le métier, l’adaptabilité</strong> et l’amour de l’apprentissage.
+                            </li>
+                            <li className='description_element'>
+                                J'ai travaillé sur de <strong>nombreux projets variés</strong>, ce qui me permet de vous proposer un <strong>service de qualité</strong> :
+                                <ul>
+                                    <li>Mise en application d'un design à partir de plans fournies.</li>
+                                    <li>Mise en place de fonctionnalités diverses.</li>
+                                    <li>Optimisation en matière de référencement (SEO) !</li>
+                                    <li>Possibilité de mise en ligne de l'application web sur un hébergement dédié pour vous !</li>
+                                </ul>
+                            </li>
                         </ul>
                     </div>
                 </section>
                 <section>
-                    <h3>Mes Compétences Techniques</h3>
+                    <h2>Mes Compétences Techniques utilisées</h2>
                     <div>
                         <div className='abilities'>
-                            <div className='abilities_element'><strong>Frontend :</strong>
-                                <ul>
-                                    <li>HTML5, CSS3, SASS, Tailwind</li>
-                                    <li>Javascript, Typescript</li>
-                                    <li>ReactJS, Redux</li>
-                                    <li>VueJS, VueX</li>
-                                </ul>
-                            </div>
-                            <div className='abilities_element'><strong>Backend :</strong>
-                                <ul>
-                                    <li>NodeJS, ExpressJS</li>
-                                    <li>MongoDB, MySQL</li>
-                                </ul>
-                            </div>
-                            <div className='abilities_element'><strong>CMS :</strong>
-                                <ul>
-                                    <li>WordPress</li>
-                                </ul>
-                            </div>
+                            <ul>
+                                <li className='abilities_element'><FaHtml5 className='skill_logo' color='darkorange' /><FaCss3 className='skill_logo' color='royalblue' /><FaSass className='skill_logo' color='deeppink' /></li>
+                                <li className='abilities_element'><SiJavascript className='skill_logo' color='gold' /><SiTypescript className='skill_logo' color='dodgerblue' /></li>
+                            </ul>
                         </div>
                     </div>
-                </section>
-                <section>
-                    <h4>Mes Formations</h4>
-                    <div>
-                        <ul className='studies'>
+                    <h3>Mes Formations</h3>
+                    <div className='studies'>
+                        <ul>
                             <li className='studies_element'>
                                 <strong>Septembre 2021 – Mai 2022</strong>
                                 <p>Titre RNCP Développeur Web - Niveau 5</p>
-                                <p>Plateforme Web OpenClassRooms</p>
+                                <p>OpenClassRooms</p>
                             </li>
                             <li className='studies_element'>
                                 <strong>2014</strong>
@@ -84,6 +72,10 @@ const Presentation = () => {
                             </li>
                         </ul>
                     </div>
+                </section>
+                <section>
+                    <h4>Mes Projets</h4>
+                    <Cards />
                 </section>
             </main>
             <Footer />
