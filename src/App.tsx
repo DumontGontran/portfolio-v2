1538ac6a4a0f1ce992175eece38095b6ed6ef483
DumontGontran <dumont.gontran@orange.fr>
2023-09-03 22:37:34 +0200

    App file updated (no more react-router-dom)

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Presentation from './pages/Presentation'
-import Projets from './pages/Projets'
 import './App.css'
 
 const App = () => {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route index path='/' element={<Presentation />}></Route>
-          <Route path='projets' element={<Projets />}></Route>
-          <Route path='/*' element={<Presentation />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <Presentation />
     </div>
   );
 }
