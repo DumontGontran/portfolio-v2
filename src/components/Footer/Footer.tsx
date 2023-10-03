0ead0fcbe592cbe8b93ec30f2ec13f6b33a7c2dc
DumontGontran <dumont.gontran@orange.fr>
2023-10-03 18:37:28 +0200

    update footer (no more links in it)

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,15 +1,9 @@
-import { FaLinkedin, FaGithubSquare } from 'react-icons/fa'
 import './Footer.css'
 
 const Footer = () => {
     return (
         <footer>
             <span>© {new Date().getFullYear()}, Dumont Gontran</span>
-            <div>
-                <a href="https://www.linkedin.com/in/gontran-dumont/" className='link link_linkedin'><FaLinkedin /></a>
-                <a href="https://github.com/DumontGontran" className='link link_github'><FaGithubSquare /></a>
-                <a href="https://www.malt.fr/profile/gontrandumont" className='link link_malt'>Malt</a>
-            </div>
         </footer>
     )
 }
