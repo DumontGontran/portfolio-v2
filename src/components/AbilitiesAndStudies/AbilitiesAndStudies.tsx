6c9b483fdea43099cf130749d217160bff7a9ac9
DumontGontran <dumont.gontran@orange.fr>
2024-01-06 20:18:54 +0100

    updated technical stack

--- src/components/AbilitiesAndStudies/AbilitiesAndStudies.tsx
+++ src/components/AbilitiesAndStudies/AbilitiesAndStudies.tsx
@@ -1,4 +1,4 @@
-import { SiHtml5, SiCss3, SiSass, SiJavascript, SiTypescript, SiReact, SiTailwindcss, SiVuedotjs } from 'react-icons/si'
+import { SiHtml5, SiCss3, SiJavascript, SiTypescript, SiReact, SiNodedotjs, SiExpress, SiMysql, SiMongodb } from 'react-icons/si'
 import '../../pages/index.css'
 import './AbilitiesAndStudies.css'
 
@@ -9,10 +9,15 @@ const AbilitiesAndStudies = () => {
                 <h4>Technical Stack</h4>
                 <div className='abilities'>
                     <ul>
+                        <span className='abilities_title'>Frontend:</span>
                         <li className='abilities_element'><SiHtml5 className='skill_logo' color='darkorange' /><SiCss3 className='skill_logo' color='royalblue' /></li>
-                        <li className='abilities_element'><SiSass className='skill_logo' color='deeppink' /><SiTailwindcss className='skill_logo' color='#00DDB8' /></li>
                         <li className='abilities_element'><SiJavascript className='skill_logo' color='gold' /><SiTypescript className='skill_logo' color='dodgerblue' /></li>
-                        <li className='abilities_element'><SiReact className='skill_logo' color='dodgerblue' /><SiVuedotjs className='skill_logo' color='black' fill='#00DDB8' strokeWidth='1.25' /></li>
+                        <li className='abilities_element'><SiReact className='skill_logo' color='dodgerblue' /></li>
+                    </ul>
+                    <ul>
+                        <span className='abilities_title'>Backend:</span>
+                        <li className='abilities_element'><SiNodedotjs className='skill_logo' color='green' /><SiExpress className='skill_logo' color='black' /></li>
+                        <li className='abilities_element'><SiMysql className='skill_logo' color='black' /><SiMongodb className='skill_logo' color='green' /></li>
                     </ul>
                 </div>
             </section>
