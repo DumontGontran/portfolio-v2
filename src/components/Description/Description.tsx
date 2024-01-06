60fb2bb1e2dfdaede25173f7d3588aba1f731252
DumontGontran <dumont.gontran@orange.fr>
2024-01-06 20:18:38 +0100

    frontend dev => fullstack dev

--- src/components/Description/Description.tsx
+++ src/components/Description/Description.tsx
@@ -5,7 +5,7 @@ import photo from '../../images/dumont-gontran.webp'
 const Description = () => {
     return (
         <section>
-            <h1>Front-End Developper/Web Integrator</h1>
+            <h1>Fullstack Developper</h1>
             <div className='presentation'>
                 <img src={photo} alt='dumont-gontran' className='photo' />
                 <ul className='description'>
@@ -13,7 +13,7 @@ const Description = () => {
                         My name is Gontran DUMONT, I am 30 years old, I live in France and I have 2 years of experience in web development.
                     </li>
                     <li className='description_element'>
-                        I have significant experience in web integration as well as functionality development.
+                        I have significant experience in frontend and backend as well as functionality development.
                     </li>
                     <li className='description_element'>
                         I am curious, rigorous, persevering, and I am able to easily adapt to change.
