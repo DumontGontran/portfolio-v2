277b6407fd7484720613828459425c29a6aba1b9
DumontGontran <dumont.gontran@orange.fr>
2024-01-06 20:34:48 +0100

    clean the store during changes when maintenance

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,17 +1,16 @@
 import { createSlice, configureStore } from '@reduxjs/toolkit'
-import pokedex from '../images/pokedex.webp'
 
 const projetsSlice = createSlice({
   name: 'Projets',
   initialState: {
     projets: [{
       id: 1,
-      title: 'Pokedex Classic',
-      logo: pokedex,
-      alt: 'logo-projet-pokedex-classic',
-      technos: 'HTML5, CSS3, Typescript',
-      description: 'Online pokedex listing the first 151 pokemons',
-      link: 'https://pokedex.gontrandev.fr'
+      title: '',
+      logo: '',
+      alt: '',
+      technos: '',
+      description: '',
+      link: ''
     }]
   },
   reducers: {
